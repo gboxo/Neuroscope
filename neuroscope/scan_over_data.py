fbda422323d547d9f16f632c8d0a8f814207240f
gboxo <gboxocor7@alumnes.ub.edu>
2024-04-15 18:28:05 +0200

    Add suport for residual stream SAE

--- neuroscope/scan_over_data.py
+++ neuroscope/scan_over_data.py
@@ -1,7 +1,16 @@
 # %%
 
 from utils import *
+import json
+import os
+import torch
+from huggingface_hub import hf_hub_download, list_files_info
+from safetensors import safe_open
+from tqdm import tqdm
 
+from sae_lens.training.config import LanguageModelSAERunnerConfig
+from sae_lens.training.sparse_autoencoder import SparseAutoencoder
+import torch.nn.functional as F
 torch.set_grad_enabled(False)
 
 # Code to automatically update the HookedTransformer code as its edited without restarting the kernel
@@ -16,7 +25,8 @@ class Config:
     n_ctx: int = 32
     overwrite: bool = True
     use_pred_log_probs: bool = False
-    use_max_neuron_act: bool = True
+    use_max_neuron_act: bool = False
+    use_feature_max_act: bool = True
     use_neuron_logit_attr: bool = False
     use_head_logit_attr: bool = False
     use_activation_stats: bool = False
@@ -58,6 +68,7 @@ else:
     new_config = {
         "debug": True,
         "use_activation_stats": True,
+        "use_feature_max_act": True,
         "model_name":"gpt2-small",
         "data_name":"pile-10k"
     }
@@ -203,6 +214,41 @@ class NeuronMaxAct(BaseMaxTracker):
         for layer, store in enumerate(self.stores):
             store.save(folder_name=str(layer), dir=self.save_dir)
         print(f"Saved {self.name} stores to:", self.save_dir)
+
+
+class FeatureResidMaxAct(BaseMaxTracker):
+    def __init__(self, cfg: Config, model: HookedTransformer, encoders: list[SparseAutoencoder]):
+        super().__init__(cfg, model, name="feature_max_act")
+
+        self.stores = []
+        for layer in range(self.model.cfg.n_layers):
+            encoder = encoders[layer]
+            store = MaxStore(self.cfg.neuron_top_k, encoder.W_enc.detach().size(1),device=self.cfg.device)
+            self.stores.append(store)
+
+            def update_max_act_hook(neuron_acts, hook, store,encoder):
+                feature_bias = encoder.b_enc
+                x_cent  = neuron_acts - encoder.b_dec
+                feat_acts_pre = einops.einsum(x_cent,encoder.W_enc,"batch seq d_in, d_in feats -> batch seq feats")
+                feat_acts = F.relu(feat_acts_pre + feature_bias)
+                store.batch_update(
+                    torch.max(feat_acts, dim=1, keepdim=False)
+                )
+
+            if self.model.cfg.act_fn in ["gelu", "relu", "gelu_new"]:
+                hook_fn = partial(update_max_act_hook, store=store,encoder = encoder)
+                self.model.blocks[layer].hook_resid_pre.add_hook(hook_fn)
+            else:
+                raise ValueError(f"Invalid Act Fn: {self.model.cfg.act_fn}")
+
+    def save(self):
+        self.save_dir.mkdir(exist_ok=True)
+        for layer, store in enumerate(self.stores):
+            store.save(folder_name=str(layer), dir=self.save_dir)
+        print(f"Saved {self.name} stores to:", self.save_dir)
+
+
+
 
 
 class HeadLogitAttr(BaseMaxTracker):
@@ -379,45 +425,84 @@ class ActivationStats:
         self.save()
 
 
-# %%
-if not cfg.debug:
-    wandb.init(config=cfg.to_dict())
-model = HookedTransformer.from_pretrained(cfg.model_name)  # type: ignore
-dataset = get_dataset(cfg.data_name)
-if len(dataset) * cfg.n_ctx < cfg.max_tokens or cfg.max_tokens < 0:
-    print("Resetting max tokens:", cfg.max_tokens, "to", len(dataset) * cfg.n_ctx)
-    cfg.max_tokens = len(dataset) * cfg.n_ctx
+def get_gpt2_res_jb_saes() -> (
+    tuple[dict[str, SparseAutoencoder], dict[str, torch.Tensor]]
+):
 
-trackers = []
-if cfg.use_head_logit_attr:
-    trackers.append(HeadLogitAttr(cfg, model))
+    GPT2_SMALL_RESIDUAL_SAES_REPO_ID = "jbloom/GPT2-Small-SAEs-Reformatted"
+    GPT2_SMALL_RESIDUAL_SAES_HOOK_POINTS = [
+        f"blocks.{layer}.hook_resid_pre" for layer in range(12)
+    ] + ["blocks.11.hook_resid_post"]
 
-if cfg.use_max_neuron_act:
-    trackers.append(NeuronMaxAct(cfg, model))
+    saes = {}
+    for hook_point in tqdm(GPT2_SMALL_RESIDUAL_SAES_HOOK_POINTS):
+        # download the files required:
+        FILENAME = f"{hook_point}/cfg.json"
+        hf_hub_download(repo_id=GPT2_SMALL_RESIDUAL_SAES_REPO_ID, filename=FILENAME)
 
-if cfg.use_neuron_logit_attr:
-    trackers.append(NeuronLogitAttr(cfg, model))
+        FILENAME = f"{hook_point}/sae_weights.safetensors"
+        path = hf_hub_download(
+            repo_id=GPT2_SMALL_RESIDUAL_SAES_REPO_ID, filename=FILENAME
+        )
 
-if cfg.use_pred_log_probs:
-    trackers.append(PredLogProbs(cfg, model))
 
-if cfg.use_activation_stats:
-    trackers.append(ActivationStats(cfg, model))
+        # Then use our function to download the files
+        folder_path = os.path.dirname(path)
+        sae = SparseAutoencoder.load_from_pretrained(folder_path)
+        saes[hook_point] = sae
 
-# %%
-try:
-    with torch.autocast(str(cfg.device), torch.bfloat16):
-        for index in tqdm(range(0, cfg.max_tokens // cfg.n_ctx, cfg.batch_size)):  # type: ignore
-            tokens = dataset[index : index + cfg.batch_size].to(cfg.device)  # type: ignore
+    return saes
 
-            logits = model(tokens).detach()
-            for tracker in trackers:
-                tracker.step(logits, tokens)
-            if not cfg.debug:
-                wandb.log({"tokens": index * cfg.n_ctx}, step=index)
-finally:
-    for tracker in trackers:
-        tracker.finish()
+
+
+
+
+
+if __name__ == "__main__":
+    
+    
+    saes = get_gpt2_res_jb_saes()
+        
+    saes = list(saes.values())
     if not cfg.debug:
-        wandb.finish()
-# %%
+        wandb.init(config=cfg.to_dict())
+    model = HookedTransformer.from_pretrained(cfg.model_name)  # type: ignore
+    dataset = get_dataset(cfg.data_name)
+    if len(dataset) * cfg.n_ctx < cfg.max_tokens or cfg.max_tokens < 0:
+        print("Resetting max tokens:", cfg.max_tokens, "to", len(dataset) * cfg.n_ctx)
+        cfg.max_tokens = len(dataset) * cfg.n_ctx
+
+    trackers = []
+    if cfg.use_head_logit_attr:
+        trackers.append(HeadLogitAttr(cfg, model))
+
+    if cfg.use_max_neuron_act:
+        trackers.append(NeuronMaxAct(cfg, model))
+
+    if cfg.use_neuron_logit_attr:
+        trackers.append(NeuronLogitAttr(cfg, model))
+
+    if cfg.use_pred_log_probs:
+        trackers.append(PredLogProbs(cfg, model))
+
+    if cfg.use_activation_stats:
+        trackers.append(ActivationStats(cfg, model))
+    if cfg.use_feature_max_act:
+        trackers.append(FeatureResidMaxAct(cfg, model,saes))
+
+    try:
+        print("1")
+        with torch.autocast(str(cfg.device), torch.bfloat16):
+            for index in tqdm(range(0, cfg.max_tokens // cfg.n_ctx, cfg.batch_size)):  # type: ignore
+                tokens = dataset[index : index + cfg.batch_size].to(cfg.device)  # type: ignore
+
+                logits = model(tokens).detach()
+                for tracker in trackers:
+                    tracker.step(logits, tokens)
+                if not cfg.debug:
+                    wandb.log({"tokens": index * cfg.n_ctx}, step=index)
+    finally:
+        for tracker in trackers:
+            tracker.finish()
+        if not cfg.debug:
+            wandb.finish()
